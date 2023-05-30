d3a05b9ed168febd25bc332a9a56063f6d852ef7
NosmanDent <olamiwale1212@gmail.com>
2023-05-31 01:42:07 +0800

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { MdAccountBalance } from "react-icons/md";
 import SideMenu from "./SideMenu";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const activeStyles = {
+    color: "white",
+  };
+
   return (
     <nav className="bg-[#182B54] py-4 inter w-full cursor-pointer">
       <div className="  w-full hidden lg:flex flex-row items-center justify-between px-10">
@@ -14,17 +18,48 @@ const Navbar = () => {
           </div>
         </Link>
 
-        <ul className="text-stone-300 font-semibold flex flex-row items-center gap-4 inter lg:text-sm text-xs ">
-          <li className="hover:border-b-2 border-white pb-2" id="market">
+        <div className="text-stone-300 font-semibold flex flex-row items-center gap-4 inter lg:text-sm text-xs ">
+          <NavLink
+            to="/"
+            className="hover:border-b-2 border-white pb-2"
+            id="market"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
             MARKETING
-          </li>
-          <li className="hover:border-b-2 border-white pb-2">ABOUT</li>
-          <li className="hover:border-b-2 border-white pb-2">VIDEO</li>
-          <li className="hover:border-b-2 border-white pb-2">OUR BOOK</li>
-          <Link to="contact">
-            <li className="hover:border-b-2 border-white pb-2">CONTACT</li>
-          </Link>
-        </ul>
+          </NavLink>
+
+          <NavLink
+            to="/"
+            className="hover:border-b-2 border-white pb-2"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            ABOUT
+          </NavLink>
+
+          <NavLink
+            to="/"
+            className="hover:border-b-2 border-white pb-2"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            VIDEO
+          </NavLink>
+
+          <NavLink
+            to="/"
+            className="hover:border-b-2 border-white pb-2"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            OUR BOOK
+          </NavLink>
+
+          <NavLink
+            to="contact"
+            className="hover:border-b-2 border-white pb-2"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            CONTACT
+          </NavLink>
+        </div>
 
         <Link to="contact">
           <div className="bg w-40 font-semibold flex items-center justify-center px-12 py-3 text-white rounded-full whitespace-nowrap">
--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import { MdAccountBalance } from "react-icons/md";
 import { BiMenuAltRight } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const SideMenu = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
+
   return (
     <nav className="lg:hidden flex w-full flex-row items-center justify-between px-6">
       <div className="flex flex-row items-center  ">
         <MdAccountBalance className="xs:text-2xl text-xl text-white" />
         <h1 className="text-white xs:text-xl text-lg">crackIt</h1>
       </div>
-
-      <div className="bg xs:w-28 w-20 font-semibold flex items-center justify-center px-6 xs:text-xs text-[10px] xs:py-3 py-2 text-white rounded-full whitespace-nowrap">
-        TALK TO US
-      </div>
+      <Link to="contact">
+        <div className="bg xs:w-28 w-20 font-semibold flex items-center justify-center px-6 xs:text-xs text-[10px] xs:py-3 py-2 text-white rounded-full whitespace-nowrap">
+          TALK TO US
+        </div>
+      </Link>
 
       <div>
         <BiMenuAltRight
@@ -32,13 +34,40 @@ const SideMenu = () => {
             />
             <div className="text-white flex flex-row items-center justify-center my-auto">
               <ul className="text-stone-300 font-semibold flex flex-col items-center gap-4 inter text-lg">
-                <li className="hover:border-b-2 border-white pb-2" id="market">
+                <li
+                  className="hover:border-b-2 border-white pb-2"
+                  id="market"
+                  onClick={() => setToggleMenu(false)}
+                >
                   MARKETING
                 </li>
-                <li className="hover:border-b-2 border-white pb-2">ABOUT</li>
-                <li className="hover:border-b-2 border-white pb-2">VIDEO</li>
-                <li className="hover:border-b-2 border-white pb-2">OUR BOOK</li>
-                <li className="hover:border-b-2 border-white pb-2">CONTACT</li>
+                <li
+                  className="hover:border-b-2 border-white pb-2"
+                  onClick={() => setToggleMenu(false)}
+                >
+                  ABOUT
+                </li>
+                <li
+                  className="hover:border-b-2 border-white pb-2"
+                  onClick={() => setToggleMenu(false)}
+                >
+                  VIDEO
+                </li>
+                <li
+                  className="hover:border-b-2 border-white pb-2"
+                  onClick={() => setToggleMenu(false)}
+                >
+                  OUR BOOK
+                </li>
+
+                <Link to="contact">
+                  <li
+                    className="hover:border-b-2 border-white pb-2"
+                    onClick={() => setToggleMenu(false)}
+                  >
+                    CONTACT
+                  </li>
+                </Link>
               </ul>
             </div>
           </div>
