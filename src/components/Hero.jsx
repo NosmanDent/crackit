b416cb6055e3df9075c01eb60be983617e44c7ab
NosmanDent <olamiwale1212@gmail.com>
2023-05-31 01:55:29 +0800

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,19 +5,25 @@ const Hero = () => {
   return (
     <main className="app__bg  h-screen  relative">
       <div className="absolute bg-black/50 w-full h-screen"></div>
-      <div className="relative flex flex-col items-center justify-center m-auto text-center gap-10 pt-32 h-screen">
+      <div className="relative flex flex-col items-center justify-center m-auto text-center gap-10  h-screen">
         <h1 className="text-white text-xl md:text-2xl tracking-wider mx-10 md:mx-4 font-semibold inter">
           Recognized as the #1 Law Firm Growth Company in America
         </h1>
-        <h1 className="text-white text-3xl xs:text-4xl md:text-5xl lg:text-6xl mx-10 md:mx-5 tracking-wider">
-          <span className="font-bold">Unreasonable Growth</span> for
-          Uncompromising Law Firms
+        <h1 className="text-white text-3xl xs:text-4xl md:text-5xl lg:text-6xl mx-10 md:mx-5 tracking-wider md:flex flex-col hidden">
+          <span className="font-bold">
+            Unreasonable Growth <br />
+          </span>{" "}
+          for Uncompromising Law Firms
+        </h1>
+
+        <h1 className="text-white text-2xl font-bold mx-10  tracking-wider md:hidden flex">
+          Unreasonable Growth for Uncompromising Law Firms
         </h1>
         <div className="flex flex-col md:flex-row gap-3 items-center">
-          <button className="bg text-white md:text-3xl text-xl font-semibold py-3 md:px-8 px-20 rounded-full">
+          <button className="bg text-white md:text-3xl text-lg font-semibold md:py-3 py-2 w-full whitespace-nowrap md:px-10 px-8 rounded-full">
             LEARN MORE
           </button>
-          <button className="bg text-white md:text-3xl text-xl font-semibold py-3 md:px-10 px-[85px] rounded-full">
+          <button className="bg text-white md:text-3xl text-lg font-semibold md:py-3 py-2 w-full whitespace-nowrap md:px-12 px-10 rounded-full">
             TALK TO US{" "}
           </button>
         </div>
